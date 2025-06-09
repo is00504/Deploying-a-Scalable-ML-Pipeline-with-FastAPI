fd3c42bc580f64386ed23489a78e4289988dc89f
isims5 <isims5@wgu.edu>
2025-06-08 22:19:00 -0400

    Add three unit tests to test_ml.py

--- test_ml.py
+++ test_ml.py
@@ -1,28 +1,47 @@
 import pytest
+import numpy as np
+from sklearn.ensemble import RandomForestClassifier
+
+from ml.model import compute_model_metrics, inference
+
+
 # TODO: add necessary import
 
 # TODO: implement the first test. Change the function name and input as needed
-def test_one():
+def test_model_metrics():
     """
-    # add description for the first test
+    # tests that model metrics are computing as expected
     """
-    # Your code here
+    labels = [1, 1, 0]
+    model_output = [0, 1, 1]
+    precision, recall, fbeta = compute_model_metrics(labels, model_output)
+    assert precision is not None and 0 <= precision <= 1
+    assert recall is not None and 0 <= recall <= 1
+    assert fbeta is not None and 0 <= fbeta <= 1
+
     pass
 
 
 # TODO: implement the second test. Change the function name and input as needed
-def test_two():
+def test_feature_num():
     """
-    # add description for the second test
+    # tests that the model has 14 features
     """
-    # Your code here
+    X = np.random.rand(3,14)
+    assert X.shape[1] == 14, f"Expected 14 features, got {X.shape[1]}."
     pass
 
 
 # TODO: implement the third test. Change the function name and input as needed
-def test_three():
+def test_inference():
     """
-    # add description for the third test
+    # tests that the inference function output returns the correct length.
     """
-    # Your code here
+    X_test = np.random.rand(3, 4)
+    y_test = [0, 1, 0]
+    model = RandomForestClassifier()
+    model.fit(X_test,y_test)
+    predictions = inference(model, X_test)
+    assert len(predictions) == len(X_test), "Output length does not match input data."
+
     pass
